11a446fee8ed20892ae85b51b41a665d5afe4429
Leon <lb.lb2@hotmail.de>
2022-07-05 11:36:58 +0200

    Specify app should list restaurants.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
 export default function App() {
-  return(
-      <div>
-        Hello, world.
-      </div>
-  )
+  return <div>Hello, world.</div>;
 }

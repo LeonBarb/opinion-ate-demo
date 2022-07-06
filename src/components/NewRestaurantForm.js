8101689fc1b6a2b0c92e2b3d6fca13dab2f7805b
Leon <lb.lb2@hotmail.de>
2022-07-06 12:10:20 +0200

    Making the first inside-Test pass.

---
+++ src/components/NewRestaurantForm.js
@@ -0,0 +1,29 @@
+import {TextField} from '@mui/material';
+import {useState} from 'react';
+import {Button} from '@mui/material';
+
+export function NewRestaurantForm({createRestaurant}) {
+  const [name, setName] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    createRestaurant(name);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        value={name}
+        onChange={e => setName(e.target.value)}
+        placeholder="Add Restaurant"
+        fullWidth
+        variant="filled"
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Add
+      </Button>
+    </form>
+  );
+}
+
+export default NewRestaurantForm;
